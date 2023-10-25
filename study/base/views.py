6f7d2cdfd7d8ffe0740c6c69cabe2c6464b2dba6
JohannHelbert <johannhelbert@gmail.com>
2023-10-25 08:52:34 +0000

    Add User Logout

--- study/base/views.py
+++ study/base/views.py
@@ -34,9 +34,13 @@ def loginPage(request):
         else:
             messages.error(request, 'Username OR password does not exist')
 
-
     context = {}
     return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
