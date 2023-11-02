7e0b37362c48fe7d942103d042d8c07659d016ec
JohannHelbert <johannhelbert@gmail.com>
2023-11-02 18:47:16 +0000

    Fix issue value error at room function

--- base/views.py
+++ base/views.py
@@ -82,7 +82,7 @@ def home(request):
                'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
-
+@login_required(login_url='login')
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all()
@@ -101,7 +101,7 @@ def room(request, pk):
                'participants': participants}
     return render(request, 'base/room.html', context)
 
-
+@login_required(login_url='login')
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
     rooms = user.room_set.all()
