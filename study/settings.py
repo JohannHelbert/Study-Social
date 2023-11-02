9c9461f7a8b72996c3ed3e5d81f58c2646b19a14
JohannHelbert <johannhelbert@gmail.com>
2023-11-02 09:10:50 +0000

    Add new MEDIA_ROOT PATH

--- study/settings.py
+++ study/settings.py
@@ -144,13 +144,13 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/static/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # STATIC_ROOT =
 
