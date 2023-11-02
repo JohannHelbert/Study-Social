e9b559142d6f70005615cab03ff7960207fcb8ec
JohannHelbert <johannhelbert@gmail.com>
2023-11-02 18:34:09 +0000

    Change allowed host link

--- study/settings.py
+++ study/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['study-social-8e0c28744b94.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['study-social-8e0c28744b94.herokuapp.com', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://study-social-8e0c28744b94.herokuapp.com/']
 
