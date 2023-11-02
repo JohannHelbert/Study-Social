951e4cf5ec3d7fb38189d3c1fdffb090f9212f3c
JohannHelbert <johannhelbert@gmail.com>
2023-11-02 09:01:58 +0000

    delete static root

--- study/settings.py
+++ study/settings.py
@@ -20,7 +20,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 # Quick-start development settings - unsuitable for production
