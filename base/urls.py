71194c2e0322639843ad03f805e4dc2822cf88f9
JohannHelbert <johannhelbert@gmail.com>
2023-11-03 09:43:54 +0000

    Fix Style issue for deployment

--- base/urls.py
+++ base/urls.py
@@ -20,3 +20,5 @@ urlpatterns = [
     path('topics/', views.topicsPage, name="topics"),
     path('activity/', views.activityPage, name="activity"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
