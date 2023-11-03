669e4338ea3a4d4acf88eee96bc32b83131038df
JohannHelbert <johannhelbert@gmail.com>
2023-11-03 10:25:28 +0000

    Change Debug to False

--- study/settings.py
+++ study/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['study-social-8e0c28744b94.herokuapp.com', 'localhost']
 
