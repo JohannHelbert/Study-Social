c6f42445e3d8239ed6b064f41973e00daec35358
JohannHelbert <johannhelbert@gmail.com>
2023-11-03 10:04:10 +0000

    Import settings conf

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
