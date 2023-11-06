7ef018a9d6b6f2ca6e1349fec322453871044ac6
JohannHelbert <johannhelbert@gmail.com>
2023-11-06 15:04:12 +0000

    Change avatar.jpg to avatar.png

--- base/models.py
+++ base/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
-    avatar = models.ImageField(null=True, default="avatar.png")
+    avatar = models.ImageField(null=True, default="../avatar.png")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
--- study/settings.py
+++ study/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['study-social-8e0c28744b94.herokuapp.com', 'localhost', '8000-johannhelbe-studysocial-3ov75r4zavg.ws-eu105.gitpod.io']
 
